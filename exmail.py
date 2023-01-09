6a66f376550b37c39b572a884235f7157c4cd852
layred <layred.dota2@mail.ru>
2023-01-09 12:44:43 +0300

    Add printing labels by printer

--- exmail.py
+++ exmail.py
@@ -7,6 +7,8 @@ import openpyxl
 import js2py
 import os
 from dotenv import dotenv_values
+import cups
+
 
 init()
 config = dotenv_values('.env')
@@ -17,6 +19,8 @@ start_message = f"""
 [2] - Расставить посылки по ячейкам
 [3] - Получить смс от отправления
 [4] - Выдать посылку если забыли сказать SMS-код
+[5] - Где посылка?
+[6] - Печать этикетки
 """
 parse_code = js2py.eval_js("""
 function parse_code(code) {
@@ -27,6 +31,29 @@ function parse_code(code) {
 }
 """)
 BASE_DIR = Path(__file__).resolve().parent
+SHIPMENT_STATUS_VALUES = {
+  0: "Импортирован",
+  10: "Принят от отправителя",
+  20: "Готов к отгрузке",
+  30: "Зарегистрирована накладная",
+  40: "Выдано курьеру",
+  45: "В пути из ПВЗ",
+  50: "Прибыл в РЦ",
+  60: "Передано в транзит \"Город отправления\" - \"Город получения\"",
+  70: "Поступил в РЦ город получения",
+  75: "В пути на ПВЗ",
+  80: "Готов к выдаче",
+  90: "Выдано",
+  100: "Отменен",
+  110: "Возврат",
+  140: "Не прибыл на ПВЗ (недостача)",
+  150: "Излишек",
+  151: "Засыл",
+  91: "Выдано частично"
+}
+
+labelPrinter = "_Label_Printer_2"
+invoicePrinter = "HP_LaserJet_Pro_MFP_M127fn"
 
 
 def build_headers(session: requests.Session):
@@ -91,6 +118,22 @@ def dump_shipment(session: requests.Session, shipments, sending):
     return session.put("https://pvz.exmail24.ru/api/sendings/" + str(sending), headers=build_headers(session), cookies=session.cookies, data=json.dumps({"shipments": shipments}))
 
 
+def get_ceils(session: requests.Session):
+    return session.get("https://pvz.exmail24.ru/api/ceils?", headers=build_headers(session), cookies=session.cookies)
+
+
+def get_sticker(session: requests.Session, shipment_id):
+    return session.get(f"https://pvz.exmail24.ru/api/etic-pdf?shipment_id={shipment_id}", headers=build_headers(session), cookies=session.cookies)
+
+
+def get_acceptance(session: requests.Session, shipment_id):
+    return session.get(f"https://pvz.exmail24.ru/api/receipt_avito_acceptance?shipment_id={shipment_id}", headers=build_headers(session), cookies=session.cookies)
+
+
+def get_shipment_russian(shipment_id):
+    return requests.get(f"https://sa.exmail24.ru/api/shipments/get-id/{shipment_id}")
+
+
 def sort_accept(freight_id):
     wb = openpyxl.load_workbook(F'{BASE_DIR}/input/accept.xlsx')
     sheet = wb.active
@@ -118,14 +161,13 @@ def sort_accept(freight_id):
                 print(Fore.LIGHTCYAN_EX + f"{shipment_json['number']}({shipment_json['id']}) {shipment_json['ceil']['name']} успешно размещена")
 
 
-def sort_send(send_id):
+def sort_send(session, send_id):
     wb = openpyxl.load_workbook(F'{BASE_DIR}/input/add.xlsx')
     sheet = wb.active
     shipments_to_add = []
     for row in range(1, sheet.max_row+1):
         if sheet.cell(row=row, column=1).value is not None:
             shipments_to_add.append(parse_code(str(sheet.cell(row=row, column=1).value)))
-    session = login()
     for i in range(0, len(shipments_to_add), 8):
         response = dump_shipment(session, shipments_to_add[i:i+8], send_id)
         if response.status_code == 200:
@@ -134,7 +176,38 @@ def sort_send(send_id):
             print(Fore.RED + f"[ERROR] {response.text}")
 
 
-if __name__ == '__main__':
+def get_sticker_file(session, shipment_id):
+    sticker = get_sticker(session, shipment_id)
+    with open(f"{BASE_DIR}/temp/sticker.pdf", 'wb') as file:
+        file.write(sticker.content)
+
+
+def get_invoice_file(session, shipment_id):
+    invoice = get_acceptance(session, shipment_id)
+    with open(f"{BASE_DIR}/temp/invoice.xlsx", 'wb') as file:
+        file.write(invoice.content)
+
+
+def print_sticker_file():
+    conn = cups.Connection()
+    labelPrinterOptions = {
+        "PrintSpeed": "60",
+        "PageSize": "w295h417",
+        "Darkness": "5",
+        "Vertical": "5"
+    }
+    conn.printFile(labelPrinter, f"{BASE_DIR}/temp/sticker.pdf", "Label", labelPrinterOptions)
+    os.remove(f"{BASE_DIR}/temp/sticker.pdf")
+
+
+def print_invoice_file():
+    pass
+    # TODO: печать накладной
+    # conn.printFile(invoicePrinter, f"{BASE_DIR}/temp/invoice.pdf", "Invoice", {})
+    # os.remove(f"{BASE_DIR}/temp/invoice.xlsx")
+
+
+def main():
     try:
         if config.get("EXMAIL_PASSWORD", False) is False or config.get("EXMAIL_PASSWORD", False) == '' or config.get("EXMAIL_LOGIN", False) is False or config.get("EXMAIL_LOGIN", False) == '':
             print(Fore.RED + "[ERROR] Не указанны переменный в файле .env, укажите логин(почту) и пароль от exmail")
@@ -143,28 +216,23 @@ if __name__ == '__main__':
         while True:
             try:
                 task_type = int(input(start_message))
-                if task_type not in range(1, 5):
-                    raise ValueError
                 session = login(login_data)
                 if task_type == 1:
-                    print(input(Fore.LIGHTWHITE_EX + "[INFO] Вы выбрали [1] - Добавить задания в отправку\n\nУбедитесь что файл add.xlsx лежит в папке с скриптом, в таблице в первом столбике подряд должны идти шк отправлений, проще всего отсканировать сканнером в таблицу.\n\n" + Fore.WHITE + "(для выхода назад напишите 'Назад')\n\n"))
+                    print(Fore.LIGHTWHITE_EX + "[INFO] Вы выбрали [1] - Добавить задания в отправку\n\nУбедитесь что файл add.xlsx лежит в папке с скриптом, в таблице в первом столбике подряд должны идти шк отправлений, проще всего отсканировать сканнером в таблицу.\n\n" + Fore.WHITE + "(для выхода назад напишите 'Назад')\n\n")
                     while True:
                         try:
                             warehouse_id = input(Fore.LIGHTWHITE_EX + "[INPUT DATA] Введите номер отправки: ")
                             if str(warehouse_id).lower() == 'назад':
                                 break
-                            response = get_warehouse(session, warehouse_id)
-                            if response.status_code == 404:
-                                raise ValueError
                             print(Fore.LIGHTBLUE_EX + "[INFO] Обрабатываю Excel файл с ШК посылок на отправку")
-                            sort_send(warehouse_id)
+                            sort_send(session, warehouse_id)
                             print(Fore.GREEN + "[SUCCESS] Обработка завершена")
                             break
                         except ValueError:
                             print(Fore.LIGHTRED_EX + '[ERROR] Неверный номер отправки, убедитесь что вы вводите номер отправки который указан в ссылке, попробуйте еще раз' + Fore.RESET)
                             continue
                 elif task_type == 2:
-                    print(input(Fore.LIGHTWHITE_EX + "[INFO] Вы выбрали [2] - Расставить посылки по ячейкам\n\nУбедитесь что файл accept.xlsx лежит в папке с скриптом, в таблице в первом столбике подряд должны чередоваться шк отправлений и шк половок(пример: 1000010143130, 4000000001379), проще всего отсканировать сканнером в таблицу.\n\n" + Fore.WHITE + "(для выхода назад напишите 'Назад')\n\n"))
+                    print(Fore.LIGHTWHITE_EX + "[INFO] Вы выбрали [2] - Расставить посылки по ячейкам\n\nУбедитесь что файл accept.xlsx лежит в папке с скриптом, в таблице в первом столбике подряд должны чередоваться шк отправлений и шк половок(пример: 1000010143130, 4000000001379), проще всего отсканировать сканнером в таблицу.\n\n" + Fore.WHITE + "(для выхода назад напишите 'Назад')\n\n")
                     while True:
                         try:
                             freight_id = input(Fore.LIGHTWHITE_EX + "[INPUT DATA] Введите номер перевозки: ")
@@ -234,6 +302,54 @@ if __name__ == '__main__':
                         except ValueError:
                             print(Fore.LIGHTRED_EX + '[ERROR] Неверное шк отправления, попробуйте еще раз' + Fore.RESET)
                             continue
+                elif task_type == 5:
+                    print(Fore.LIGHTWHITE_EX + "[INFO] Вы выбрали [5] - Где посылка?\n\n" + Fore.WHITE + "(для выхода назад напишите 'Назад')\n\n")
+                    while True:
+                        try:
+                            shipment_id = input(Fore.LIGHTWHITE_EX + "[INPUT] Введите шк отправления: ")
+                            if str(shipment_id).lower() == 'назад':
+                                break
+                            shipment_id = int(shipment_id)
+                            if len(str(shipment_id)) == 13:
+                                shipment_id = parse_code(str(shipment_id))
+                            shipment = get_shipment(session=session, shipment_id=shipment_id)
+                            if shipment.status_code == 404:
+                                raise ValueError
+                            ceils = get_ceils(session).json()
+                            if shipment.json()['ceil_id'] is not None:
+                                for ceil in ceils['ceils']:
+                                    if ceil['id'] == shipment.json()['ceil_id']:
+                                        print(Fore.LIGHTGREEN_EX + f'[SUCCESS] Отправление: {shipment.json()["number"]} - полка {ceil["name"]}')
+                                        break
+                            else:
+                                print(Fore.LIGHTRED_EX + f'[ERROR] У отправления {shipment.json()["number"]} нет полки, статус {SHIPMENT_STATUS_VALUES[int(shipment.json()["status"])]}' + Fore.RESET)
+                        except ValueError:
+                            print(Fore.LIGHTRED_EX + '[ERROR] Неверное шк отправления, попробуйте еще раз' + Fore.RESET)
+                            continue
+                elif task_type == 6:
+                    print(Fore.LIGHTWHITE_EX + "[INFO] Вы выбрали [6] - Печать этикетки\n\n" + Fore.WHITE + "(для выхода назад напишите 'Назад')\n\n")
+                    while True:
+                        try:
+                            shipment_id = input(Fore.LIGHTWHITE_EX + "[INPUT] Введите шк отправления(как обычного так и почты россии): ")
+                            if str(shipment_id).lower() == 'назад':
+                                break
+                            shipment_id = int(shipment_id)
+                            shipment_response = get_shipment(session, shipment_id)
+                            if shipment_response.status_code == 404:
+                                shipment_response = get_shipment_russian(shipment_id)
+                                if shipment_response.json() == {}:
+                                    raise ValueError
+                                else:
+                                    get_sticker_file(session, int(shipment_response.text))
+                                    print_sticker_file()
+                            else:
+                                get_sticker_file(session, int(shipment_id))
+                                print_sticker_file()
+                        except ValueError:
+                            print(Fore.LIGHTRED_EX + '[ERROR] Неверное шк отправления, попробуйте еще раз' + Fore.RESET)
+                            continue
+                else:
+                    raise ValueError
             except ValueError:
                 os.system('clear')
                 print(Fore.LIGHTRED_EX + 'Неверное число, попробуйте еще раз' + Fore.RESET)
@@ -241,3 +357,20 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         print(Fore.RED + "\n\n[EXITED] Выполнение программы прекращено")
         exit()
+
+
+if __name__ == '__main__':
+    main()
+
+    # conn = cups.Connection()
+    # printers = conn.getPrinters()
+    # emptyDict = {
+    #     "PrintSpeed": "60",
+    #     "PageSize": "w295h417",
+    #     "Darkness": "5",
+    #     "Vertical": "5"
+    # }
+    # AvailablePrinters = list(printers.keys())
+    # PrinterUsing = AvailablePrinters[0]
+    # conn.printFile(PrinterUsing, f"{BASE_DIR}/sticker.pdf", "Custom.100x70", emptyDict)
+    # os.remove(f"{BASE_DIR}/temp/sticker.pdf")
